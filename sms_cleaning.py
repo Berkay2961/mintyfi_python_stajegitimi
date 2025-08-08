6717aef79ce3db1ecd1896151da0c44b800b9dcf
Berkay2961 <156474257+Berkay2961@users.noreply.github.com>
2025-08-08 15:15:31 +0300

    Delete sms_cleaning.py

--- sms_cleaning.py
+++
@@ -1,8 +0,0 @@
-import pandas as pd
-
-# Veri setini indir
-url = "https://raw.githubusercontent.com/justmarkham/pycon-2016-tutorial/master/data/sms.tsv"
-df = pd.read_table(url, header=None, names=['label', 'message'])
-
-print("İlk 5 veri:")
-print(df.head())
